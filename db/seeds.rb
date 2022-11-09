d05910771462df60f4fdfdabe5c90bc0843f5aa7
Joaquinb2000 <joaquinberon2000@gmail.com>
2022-11-09 02:12:02 -0400

    Creating user and message tables with models. Seeded the database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do
+  user = User.create(username: Faker::Internet.user[:username], password: "password")
+  5.times do
+    user.messages.create(body: Faker::Marketing.buzzwords)
+  end
+end
+
