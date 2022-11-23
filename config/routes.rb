c4e8e9989eee80f4c49b7c921ed15b69209216f8
Joaquinb2000 <joaquinberon2000@gmail.com>
2022-11-23 12:48:24 -0400

    Changing root path

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
   get "chatroom/(:id)", to: 'chatrooms#show'
 
-  root "sessions#new"
-
-  get "login", to: 'sessions#new'
+  root to: "sessions#new"
   post "login", to: "sessions#create"
   delete "logout", to: "sessions#destroy"
 
